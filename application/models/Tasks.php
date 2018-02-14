d4d171a071c30dd8df287326636da186cd565298
MikeWeiZhou <mike.weizhou@gmail.com>
2018-02-13 16:51:18 -0800

    Completed job 3 - ordered list page features

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,5 +1,16 @@
 <?php
 
+// return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
+function orderByCategory($a, $b)
+{
+    if ($a->group < $b->group)
+        return -1;
+    elseif ($a->group > $b->group)
+        return 1;
+    else
+        return 0;
+}
+
 /**
  * Tasks model.
  */
@@ -11,5 +22,28 @@ class Tasks extends CSV_Model
     public function __construct()
     {
         parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+    }
+
+    function getCategorizedTasks()
+    {
+        // extract the undone tasks
+        foreach ($this->all() as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+
+        // substitute the category name, for sorting
+        foreach ($undone as $task)
+            $task->group = $this->app->group($task->group);
+
+        // order them by category
+        usort($undone, "orderByCategory");
+
+        // convert the array of task objects into an array of associative objects       
+        foreach ($undone as $task)
+            $converted[] = (array) $task;
+
+        return $converted;
     }
 }
